836f38f8ee8824b1591f0fdb3cf2125c965215f9
jonroby <joncaroby@gmail.com>
2018-12-04 18:17:04 -0800

    rewrite

--- mods/index.js
+++ mods/index.js
@@ -1,20 +1,24 @@
-const actionConstants = require('./actionConstants');
-const actionCreators = require('./actionCreators');
-const component = require('./component');
-const reducer = require('./reducer');
-const rootReducer = require('./rootReducer');
-const rootSaga = require('./rootSaga');
+const actionConstant = require("./actionConstant");
+const actionCreator = require("./actionCreator");
+const component = require("./component");
+const reducer = require("./reducer");
+const rootReducer = require("./rootReducer");
+const rootSaga = require("./rootSaga");
 
-const componentState = require('./componentState');
-const reducerState = require('./reducerState');
+const componentState = require("./componentState");
+const reducerState = require("./reducerState");
 
 module.exports = {
-  actionConstants,
-  actionCreators,
-  component,
-  reducer,
-  rootReducer,
-  rootSaga,
-  componentState,
-  reducerState
+  addAction: {
+    actionConstant,
+    actionCreator,
+    component,
+    reducer,
+    rootReducer,
+    rootSaga
+  },
+  addState: {
+    componentState,
+    reducerState
+  }
 };
